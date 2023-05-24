c072ee2a21e7eef764ca04e0bef4a5992bc81b5d
Jeison pedroso <jeison.contas@gmail.com>
2023-05-24 09:35:29 -0300

    Add rules create store service

--- src/modules/stores/controllers/StoreController.ts
+++ src/modules/stores/controllers/StoreController.ts
@@ -1,8 +1,13 @@
 import { Request, Response } from 'express';
+import CreateStoreService from '../services/CreateStoreService';
 
 class StoreController {
   public async create(request: Request, response: Response): Promise<Response> {
-    return response.json(request.body);
+    const storeService = new CreateStoreService();
+
+    const stored = await storeService.execute(request.body);
+
+    return response.json(stored);
   }
 }
 export default StoreController;
