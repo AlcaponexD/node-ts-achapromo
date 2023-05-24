7e3f0239fc4aad6505b24bded50c73309bf7716b
Jeison pedroso <jeison.contas@gmail.com>
2023-05-24 09:35:06 -0300

    Add rules at custom repository

--- src/modules/stores/typeorm/repository/StoreRepository.ts
+++ src/modules/stores/typeorm/repository/StoreRepository.ts
@@ -2,6 +2,16 @@ import { EntityRepository, Repository } from 'typeorm';
 import Store from '../entities/Store';
 
 @EntityRepository(Store)
-class StoreRepository extends Repository<Store> {}
+class StoreRepository extends Repository<Store> {
+  public async findByUrl(url: string): Promise<Store | undefined> {
+    const store = await this.findOne({
+      where: {
+        url,
+      },
+    });
+
+    return store;
+  }
+}
 
 export default StoreRepository;
