f474efa939e0b6d98ebd8ba76d3a07a2cc10c70e
Jeison pedroso <jeison.contas@gmail.com>
2023-07-24 13:36:33 -0300

    test executable off

--- src/modules/crawlers/Terabyte.ts
+++ src/modules/crawlers/Terabyte.ts
@@ -7,7 +7,7 @@ class Terabyte {
     //Abre o navegador
     const browser = await puppeteer.launch({
       headless: 'new',
-      //executablePath: '/usr/bin/chromium-browser',
+      executablePath: '/usr/bin/chromium-browser',
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
 
@@ -18,9 +18,9 @@ class Terabyte {
     await page.setExtraHTTPHeaders({
       'Accept-Language': 'en',
     });
-    // await page.setUserAgent(
-    //   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
-    // );
+    await page.setUserAgent(
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+    );
 
     await page.goto(url);
     await page.setViewport({ width: 1080, height: 1024 });
