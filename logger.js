4f85fa441ef4bfb8c0c9a22d0725aafca019c16b
Jeison pedroso <jeison.contas@gmail.com>
2024-02-05 10:39:47 -0300

    changed config winston logger

--- logger.js
+++ logger.js
@@ -1,21 +1,34 @@
-//logger.js
 const winston = require('winston');
+const path = require('path');
+const fs = require('fs');
 
+// Define o caminho para o diretório de logs
+const logsDirectory = path.join(__dirname, 'logs');
+
+// Cria o diretório de logs se não existir
+if (!fs.existsSync(logsDirectory)) {
+    fs.mkdirSync(logsDirectory);
+}
+
+// Configuração do logger
 const logger = winston.createLogger({
+    level: 'info',
     format: winston.format.combine(
-        winston.format.errors({ stack: true }),
+        winston.format.timestamp(),
         winston.format.json()
     ),
     transports: [
-        new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'info.log', level: 'info' }),
+        new winston.transports.File({
+            filename: path.join(logsDirectory, 'error.log'),
+            level: 'error',
+            format: winston.format.json(),
+        }),
+        new winston.transports.File({
+            filename: path.join(logsDirectory, 'info.log'),
+            level: 'info',
+            format: winston.format.json(),
+        }),
     ],
 });
-
-if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-        format: winston.format.simple()
-    }));
-}
 
 module.exports = logger;
