4f24a6b94db3a2e33c3a0e0121ffe20ad49f5df5
Jeison pedroso <jeison.contas@gmail.com>
2024-07-18 12:20:36 -0300

    help true

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,4 +1,14 @@
 import { createConnection } from 'typeorm';
 
-//Ele procura uma config ormconfig
-createConnection();
+async function startApp() {
+  try {
+    const connection: Connection = await createConnection();
+    console.log('Conexão com o banco de dados estabelecida com sucesso!');
+
+    // Outras operações com o TypeORM aqui
+  } catch (error) {
+    console.error('Erro ao conectar ao banco de dados:', error);
+  }
+}
+
+startApp();
