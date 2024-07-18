61e6159e7f3fe77f75747f3d50ac332ac9209920
Jeison pedroso <jeison.contas@gmail.com>
2024-07-18 14:20:02 -0300

    cluster test

--- pm2.config.js
+++ pm2.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: 'AchaPromoAPI',
       script: 'dist/shared/http/server.js',
       user: 'ubuntu',
+      exec_mode: 'cluster',
       watch: true,
       max_memory_restart: '24G',
       //Agora vai
