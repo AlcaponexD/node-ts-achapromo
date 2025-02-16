c34101d084a01b8b90853c2cd1ada0884c0bba84
Jeison pedroso <jeison.contas@gmail.com>
2025-02-16 15:10:47 -0300

    change published = 1

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -92,7 +92,7 @@ export default class CreateProductService {
     //   throw new AppError('Produto já existente');
     // }
 
-    data.published = publishedEnum.Option1;
+    data.published = publishedEnum.Option2;
     data.in_review = InReviewEnum.Option2;
 
     const store = await this.getStore(data.url);
